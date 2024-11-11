bb047e3f1dd5f13d2658fddc048b58b02e424582
Saif <admin@saifuddin.uk>
2024-11-11 15:34:30 +0000

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./gameProject/game1/**/*.{html,js}",
+            // "./**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
