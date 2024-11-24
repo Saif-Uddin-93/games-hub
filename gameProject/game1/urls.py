c855184a3af968f02b278f53d16184c7d791fee3
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2024-11-24 13:44:55 +0000

    added pages for about, games, contact

--- gameProject/game1/urls.py
+++ gameProject/game1/urls.py
@@ -3,6 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', home, name="home"),
+    path('about/', about, name="about"),
+    path('games/', games, name="games"),
+    path('contact/', contact, name="contact"),
     path("test/", test, name="test"),
     path("react/", react, name="react")
 ]
--- gameProject/game1/views.py
+++ gameProject/game1/views.py
@@ -19,3 +19,21 @@ def react(request):
     return render(request, 'game1/index.html', {
         "react_items" : items
     })
+
+def about(request):
+    items = []
+    return render(request, 'about.html', {
+        "about_items" : items
+    })
+
+def games(request):
+    items = []
+    return render(request, 'games.html', {
+        "games_items" : items
+    })
+
+def contact(request):
+    items = []
+    return render(request, 'contact.html', {
+        "contact_items" : items
+    })
